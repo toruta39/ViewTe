3d5986069b8d999083423f3b2808ef80412a2d55
Joshua Zhang <toruta39@users.noreply.github.com>
2017-07-23 15:55:21 +0900

    appium test (#6)
    * add appium simple test
    
    * enable e2etest on android with appium
    
    * add appium server npm script, update readme

---
+++ tests/appium/helpers/caps.js
@@ -0,0 +1,8 @@
+export const android = {
+  browserName: '',
+  'appium-version': '1.6',
+  platformName: 'Android',
+  platformVersion: '5.1',
+  deviceName: 'Android Emulator',
+  app: 'android/app/build/outputs/apk/app-release.apk'
+}
