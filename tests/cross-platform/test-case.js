76763223468887ca71e8f1eb1a00122540ff39a4
Joshua Zhang <toruta39@gmail.com>
2017-12-01 22:50:51 +0900

    tweak test code

--- tests/cross-platform/test-case.js
+++ tests/cross-platform/test-case.js
@@ -17,8 +17,8 @@ const run = (...args) => {
   });
   
   it("should get url", async () => {
-    await driver.url('https://www.google.com/ncr');
-    await driver.sleep(2000);
+    await driver.get('https://www.google.com/ncr');
+    await driver.sleep(5000);
     const url = await driver.url();
   
     expect(url.indexOf('https://www.google.com/')).toBe(0);
@@ -29,7 +29,7 @@ const run = (...args) => {
     await input.sendKeys('ViewTe');
     const button = await driver.elementByXPath('//button[@id="tsbb"]');
     await button.tap();
-    await driver.sleep(2000);
+    await driver.sleep(5000);
     const title = await driver.title();
     expect(title).toBe('ViewTe - Google Search');
   });  
--- tests/cross-platform/test-utils.js
+++ tests/cross-platform/test-utils.js
@@ -4,42 +4,43 @@ import * as caps from '../appium/helpers/caps';
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 300000;
 
-const CMD_WAIT_TIME = 500;
+const CMD_WAIT_TIME = 3000;
 
 const testUtils = {
   driver: null,
   platform: null,
 
-  async initDriver(platform = 'ios', env = 'WKWebView') {
-    if (!this.driver) {
-      const serverConfig = process.env.npm_package_config_sauce ?
-        serverConfigs.sauce : serverConfigs.local;
-  
-      this.driver = wd.promiseChainRemote(serverConfig);
-      this.platform = platform;
-  
-      const desired = Object.assign({}, caps[this.platform]);
-  
-      await this.driver.init(desired);
-    }
-    
+  async initDriver(platform, env) {
+    const serverConfig = process.env.npm_package_config_sauce ?
+      serverConfigs.sauce : serverConfigs.local;
+
+    this.driver = wd.promiseChainRemote(serverConfig);
+    this.platform = platform;
+
+    const desired = Object.assign({}, caps[this.platform]);
+
+    await this.driver.init(desired);
+    await this.driver.sleep(CMD_WAIT_TIME);
     await this.switchEnv(env);
   
     return this.driver;
   },
 
   async switchEnv(env) {
-    // TODO: handle android case
     await this.setNativeContext();
-    const menuButton = await this.driver.elementByXPath('//XCUIElementTypeOther[@name="menu-button"]');
-    await menuButton.tap();
-    const envButton = await this.driver.elementByXPath(`//XCUIElementTypeOther[@name="environment-list"]//XCUIElementTypeOther[@name="${env}"]`);
-    await envButton.tap();
-  
-    const browserEnvironment = await this.driver.elementByXPath('//XCUIElementTypeStaticText[@name="environment-name"]');
-    const text = await browserEnvironment.text();
-  
-    if (text !== env) throw new Error('env doesn\'t match');
+
+    if (this.platform === 'ios') {
+      const browserEnvironment = await this.driver.elementByXPath('//XCUIElementTypeStaticText[@name="environment-name"]');
+      const text = await browserEnvironment.text();
+
+      if (text !== env) {
+        const menuButton = await await this.driver.elementByXPath('//XCUIElementTypeOther[@name="menu-button"]');
+        await menuButton.tap();
+        const envButton = await this.driver.elementByXPath(`//XCUIElementTypeOther[@name="environment-list"]//XCUIElementTypeOther[@name="${env}"]`);
+        await envButton.tap();
+        await this.driver.sleep(CMD_WAIT_TIME);
+      }
+    }
 
     await this.setWebviewContext();
   },
